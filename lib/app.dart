a9c25f00ba3730a9a3301f296cdd88246f3a618a
sajidh <sajidhshaji4@gmail.com>
2025-04-07 13:59:26 +0530

    folders arranged

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:supermarket/views/auth/signup.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Super Market',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Signup(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:supermarket/main.dart';
+import 'package:supermarket/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
